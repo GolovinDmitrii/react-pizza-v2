863a24c03963f523da176297c80b4bde33b176db
golovindmitrii <itgolovindmitrii@gmail.com>
2025-03-22 10:24:12 +0500

    2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,29 @@
+import './scss/app.scss';
+
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock price="350" title="Мексиканская" />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
